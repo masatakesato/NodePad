64336a8fadfc0ef270c8b9f8f0be0018ad075ff3
masayan1115 <masatake1115@hotmail.co.jp>
2020-06-27 15:41:26 +0900

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 import setuptools
 
 
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
+#def _requires_from_file(filename):
+#    return open(filename).read().splitlines()
 
-print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 setuptools.setup(
-    name="mypackage",
-    version="0.0.1",
-    author="masayan",
-    author_email="masayan@example.com",
-    description="mypackage is my own python package",
-    long_description="The halloween party has gone over...",
-    long_description_content_type="text/markdown",
-    url="https://??????",
+    name = 'nodepad',
+    version = '0.0.1',
+    author = 'masayan',
+    author_email = 'me@example.com',
+    description = 'My python node editor package',
+    #long_description = 'The halloween party has gone over...',
+    #long_description_content_type='text/markdown',
+    url='https://bitbucket.org/masayan1115/nodepad',
     packages=setuptools.find_packages(),
     #install_requires=_requires_from_file('requirements.txt'),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ]
 )
